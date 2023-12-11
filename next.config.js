85b9ea8dde57d290bc6128dcb890dbe7d929c9da
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2023-12-11 22:01:14 +0100

    update base url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    PATH_URL_BACKEND: "http://localhost:3000/api",
+    PATH_URL_BACKEND: "http://localhost:3000/api/",
   },
 };
 
